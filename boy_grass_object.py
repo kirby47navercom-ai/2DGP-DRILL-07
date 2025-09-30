09ea27e20c77a0e6d5e04edcd16047ee33b0d097
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-09-30 16:12:56 +0900

    수업 끝

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -3,6 +3,48 @@ import random
 
 
 # Game object class here
+class Zombie:
+    def __init__(self):
+        self.x, self.y = 100, 170
+        self.frame = 0
+        self.image = load_image('zombie_run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 10
+        self.x += random.randint(-1, 2)
+
+    def draw(self):
+        frame_width = self.image.w // 10
+        frame_height = self.image.h
+        self.image.clip_draw(self.frame * frame_width, 0, frame_width, frame_height,
+        self.x, self.y, frame_width // 2, frame_height // 2)
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+
+class Boy:
+    def __init__(self):
+        self.x = random.randint(600, 700)
+        self.y = 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += random.randint(-2,1)
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 
 def handle_events():
@@ -17,8 +59,43 @@ def handle_events():
 
 
 
+def reset_world():
+    global running
+    running = True
+
+    global world
+
+    world = []
+    grass = Grass()
+    world.append(grass)
+
+    team = [Boy() for i in range(11)]
+    world += team
+
+    zombie = Zombie()
+    world.append(zombie)
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
 open_canvas()
-
-
+reset_world()
+while running:
+    handle_events()
+    #게임 로직
+    update_world()
+    #게임 랜더링
+    render_world()
+    delay(0.02)
 
 close_canvas()
